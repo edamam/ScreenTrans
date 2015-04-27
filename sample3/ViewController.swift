f8cb49d4cafe71dac25ba4ba9515ed8ec54266d2
Nieda Miho <noopy@110-132-150-216.rev.home.ne.jp>
2015-04-27 12:28:23 +0900

    Initial Commit

---
+++ sample3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample3
+//
+//  Created by Nieda Miho on 2015/04/27.
+//  Copyright (c) 2015年 Nieda Miho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
